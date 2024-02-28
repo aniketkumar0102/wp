8e5f5f7e4de0d79b0dccf5c363fe3e80833e721c
Aniket Kumar <122250188+aniketkumar0102@users.noreply.github.com>
2024-02-28 14:59:56 +0530

    Update index.php

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -30,7 +30,7 @@ if ( wp_is_mobile() ) {
 }
 
 // Used in the HTML title tag.
-$title       = __( 'Deepak' );
+$title       = __( 'Deepak KLabs' );
 $parent_file = 'index.php';
 
 $help  = '<p>' . __( 'Welcome to your WordPress Dashboard!' ) . '</p>';
